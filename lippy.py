c0a0fe707a2dbbcf9d06d182988a5d2b6d6307b2
Alyce <Alyceosbourne@outlook.com>
2023-01-16 20:30:55 +0000

    added usage examples

--- lippy.py
+++ lippy.py
@@ -24,28 +24,30 @@ if __name__ == '__main__':
 
         return _parse_char
 
+
     def _parse_single_word(word):
         def _parse_word(state: ParserState):
-            # if in range
             if state.pos + len(word) <= len(state.input):
                 if state.input[state.pos:state.pos + len(word)] == word:
                     return state.append(word).shift(len(word))
                 return state.error(f"Expected {word} but got {state.input[state.pos:state.pos + len(word)]}")
             return state.error(f"Expected {word} but got EOF")
+
         return _parse_word
+
 
     parse_a = Parser(_parse_single_char("a"))
     parse_b = Parser(_parse_single_char("b"))
     parse_c = Parser(_parse_single_char("c"))
+    parse_abc = Parser(_parse_single_word("abc"))
 
     parse_a_or_b = parse_a | parse_b
     parse_a_and_b = parse_a & parse_b
     parse_a_and_b_and_c = parse_a_and_b & parse_c
 
-
     Text("b") >> parse_a_or_b >> print
     Text("ab") >> parse_a_and_b >> print
     Text("abc") >> parse_a_and_b_and_c >> print
+    Text("abc") >> parse_abc >> print
 
-    print()
 
